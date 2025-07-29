b16a725cced9ca41ef790af513650847142bb862
Austin Young <austineyoung2000@gmail.com>
2025-07-29 18:40:53 -0400

    lynx: Enables Pixel overlay and configures init library
    Configures the device to ship with Pixel Overlays.
    
    Also configures the init library to be used by the device.

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -8,7 +8,8 @@ BUILD_BROKEN_DUP_RULES := true
 DISABLE_ARTIFACT_PATH_REQUIREMENTS := true 
 DEVICE_PATH := device/google/lynx
 # Init
-TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_lynx
+INIT_LIB := //$(DEVICE_PATH):libinit_lynx
+$(call soong_config_set,libinit,vendor_init_lib,$(INIT_LIB))
 
 # Kernel modules
 BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES_BLOCKLIST_FILE := $(DEVICE_PATH)/modules.blocklist.vendor_kernel_boot
