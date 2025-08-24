635ff54400058f9886dc2301a29c64ed202a76f8
Austin Young <austineyoung2000@gmail.com>
2025-08-24 19:54:30 -0400

    Revert "lynx: Removed V4A"
    This reverts commit 3373e4f67b48d9dd6c331db2a96e9d4893c81ca4.
    
    - adds back viper4android

--- device-lineage.mk
+++ device-lineage.mk
@@ -20,6 +20,9 @@ PRODUCT_PACKAGES += \
 # PixelParts
 -include packages/apps/PixelParts/device.mk
 
+# Viper4Android FX
+-include packages/apps/ViPER4AndroidFX/config.mk
+
 # PixelSupport
 include device/google/gs-common/pixelsupport/pixelsupport.mk
 
